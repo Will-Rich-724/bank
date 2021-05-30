0b1ea2568d49203e0f54899f59116f42c1a5f890
Will-Rich-724 <williamtrich5@gmail.com>
2021-05-30 18:31:49 -0500

    update delete account

--- client/src/Components/CloseAccountButton.js
+++ client/src/Components/CloseAccountButton.js
@@ -4,15 +4,25 @@ import axios from 'axios';
 import Button from 'react-bootstrap/Button'
 
 const CloseAccountButton = (props) => {
-    const {accountId, userId, balance, successCallback} = props
+    const {accountId, userId, balance, successCallback} = props;
+    const [errMessage, setErrMessage] = useState("")
     
     const deleteAccount = e => {
+        if (balance > 0) {
+            setErrMessage("Account must have zero dollars to be closed")
+            return console.log("An Account must have a balance of zero dollars to be closed")
+        }else{
         axios.delete(`http://localhost:8000/api/user/${userId}/${accountId}`)
             .then(res => {successCallback();})
+        }
     }
     
     return(
-        <Button variant="danger" onClick={deleteAccount}>Close Account</Button>
+        <span>
+            <Button variant="danger" onClick={deleteAccount}>Close Account</Button>
+            <br/>
+            {errMessage ? errMessage : null}
+        </span>
     )
 }
 
